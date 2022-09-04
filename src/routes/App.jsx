3448dd3d4c8688d80612133deddc741a64c95fe6
Monogato <monogato.dg@gmail.com>
2022-09-04 00:18:10 -0500

    ADD Home and NotFound Pages

--- src/routes/App.jsx
+++ src/routes/App.jsx
@@ -4,14 +4,16 @@ import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import '../styles/global.scss'
 //Components
 import { Layout } from '../containers/Layout';
-import { Info } from '../containers/Info';
+import { Home } from '../pages/Home';
+import { NotFound } from '../pages/NotFound';
 
 function App() {
     return(
         <BrowserRouter>
             <Layout>
                 <Routes>
-                    <Route exact path='/' element={<Info />} />
+                    <Route exact path='/' element={<Home />} />
+                    <Route path='*' element={<NotFound />}/>
                 </Routes>
             </Layout>
         </BrowserRouter>

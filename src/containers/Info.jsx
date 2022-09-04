6dbbdda575063bed4541ab233d3db534791d677e
Monogato <monogato.dg@gmail.com>
2022-09-03 23:10:26 -0500

    ADD Assets and styles of Info Container

--- src/containers/Info.jsx
+++ src/containers/Info.jsx
@@ -1,9 +1,11 @@
 import React from 'react'
+//styles
+import '../styles/containers/Info.scss'
 
 function Info() {
     return(
         <main className='info'>
-            
+
         </main>
     )
 }

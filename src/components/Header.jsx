c4a2d0cc9d04b6494e5bf5b6d9a13ca8b79ac3c1
Monogato <monogato.dg@gmail.com>
2022-09-03 22:45:57 -0500

    ADD Header Assets

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,17 +1,20 @@
 import React from 'react'
 //styles "/*Mobile First Layout*/"
 import '../styles/components/Header.scss'
+//assets
+import brandLogo from '../assets/brand/Tu-PruTec.svg'
+import iconArrowDown from '../assets/icons/Arrow-Down.svg'
 
 function Header () {
     return(
         <header className='header'>
             <div className='header__logo'>
-                <img src="" alt="logo Tu PruTec" />
+                <img src={brandLogo} alt="logo Tu PruTec" />
             </div>
             <div className='header__user'>
                 <p>
                     user name
-                    <img src="" alt="Icon Arrow Down" />
+                    <img src={iconArrowDown} alt="Icon Arrow Down" />
                 </p>
             </div>
         </header>

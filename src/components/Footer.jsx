89d481e2174487b6f8bb14e98cbab2e304956595
Monogato <monogato.dg@gmail.com>
2022-09-03 21:02:39 -0500

    ADD Footer Structure

--- src/components/Footer.jsx
+++ src/components/Footer.jsx
@@ -3,6 +3,17 @@ import React from 'react'
 function Footer() {
     return(
         <footer className='footer'>
+            <div className='footer__isotipo'>
+                <img src="" alt="isotipo" />
+            </div>
+            <div className='footer__copyright'>
+                <h6>
+                    ⓒ 2022 Novopayment Inc. All rights reserved.
+                </h6>
+            </div>
+            <div className='footer__isotipo'>
+                <img src="" alt="isotipo" />
+            </div>
         </footer>
     )
 }

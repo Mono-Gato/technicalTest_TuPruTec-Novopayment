63f4675e34641e1e412bef53fc2956beff81816d
Monogato <monogato.dg@gmail.com>
2022-09-03 22:31:39 -0500

    ADD Header component structure

---
+++ src/components/Header.jsx
@@ -0,0 +1,19 @@
+import React from 'react'
+
+function Header () {
+    return(
+        <header className='header'>
+            <div className='header__logo'>
+                <img src="" alt="logo Tu PruTec" />
+            </div>
+            <div className='header__user'>
+                <p>
+                    user name
+                    <img src="" alt="Icon Arrow Down" />
+                </p>
+            </div>
+        </header>
+    )
+}
+
+export { Header }
